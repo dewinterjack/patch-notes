c7b44ba9f16135f02958026e225100c88b3753ca
Jack Dewinter <jackdewinter1@gmail.com>
2024-05-05 17:22:54 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import dotenv
+dotenv.load_dotenv()
+from langchain_community.document_loaders import AsyncHtmlLoader
+
+urls = ["https://www.espn.com", "https://lilianweng.github.io/posts/2023-06-23-agent/"]
+loader = AsyncHtmlLoader(urls)
+docs = loader.load()
